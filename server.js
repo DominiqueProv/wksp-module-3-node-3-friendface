edf4d019903f4a92fa47e7956296a716b2a01180
Dominiqueprov <dominiquepro@hotmail.com>
2020-02-24 17:11:38 -0500

    Update final

--- server.js
+++ server.js
@@ -1,46 +1,56 @@
 'use strict'
-
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
 const PORT = process.env.PORT || 8000;
-let currentUser = null;
 const app = express();
 const { users } = require('./data/users');
+let currentUser = null;
 
 
-
-
+let getOtherFriends = (givenUser) => { 
+    let otherFriends = users.filter(user => !givenUser.friends.includes(user.id) && user.id !== givenUser.id)
+    return otherFriends
+}
 
 const handleHome = (req, res) => {
     if (!currentUser) { res.redirect('/signin'); return; }
+let otherFriends = getOtherFriends(currentUser);
+    
+    //otherFriends = otherFriends.filter(e => !newcurrentUser.id.includes(e))
     res.render('pages/homepage', {
         title: 'Welcome to Friendface',
         image: currentUser.avatarUrl,
         name: currentUser.name,
-        currentUser : currentUser,
-        users : users,
+        currentUser: currentUser,
+        users: users,
+        otherFriends,
+        friend: users.id
     });
 };
 const handleSignin = (req, res) => {
     if (currentUser) { res.redirect('/'); return; }
     res.render('pages/signin', {
         title: 'Sign in',
+        // otherFriends,
     });
 };
 
 
-
 const handleUser = (req, res) => {
-    if (!currentUser) { res.redirect('/signin'); return; }
-    const id = req.params.id;
+    if (!currentUser) { res.redirect('/signin'); return; }console.log(req.params.id )
+    const id = req.params.id !== undefined ? req.params.id : currentUser.id;
     const user = users.find(user => user.id === id)
+    const otherFriends = getOtherFriends(user)
+
     res.render('pages/homepage', {
         title: 'Welcome to Friendface',
         image: user.avatarUrl,
         name: user.name,
-        currentUser : user,
-        users,
-        user : user,
+        currentUser: user,
+        users: users,
+        otherFriends,
+        friend: users.id
     });
 };
 
@@ -58,6 +68,7 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/', handleHome)
@@ -65,6 +76,13 @@ app.get('/signin', handleSignin)
 app.get('/user/:id', handleUser)
 app.get('/getname', handleName)
 
+app.post('/addFriend', (req, res) => {
+    console.log('REQ *********:')
+    console.log(req)
+    let newFriend = req.body.newFriend;
+    currentUser.friends.push(newFriend)
+    res.redirect('/')
+});
 
 app.get('*', (req, res) => {
     res.status(404);
@@ -73,6 +91,7 @@ app.get('*', (req, res) => {
         path: req.originalUrl
     });
 });
+
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
